f6d9e65ff58a067d3f2728c79cd2ae4198f3fec2
Samrendra Barick <baricksamarendra@gmail.com>
2023-11-23 20:31:16 +0530

    Chapter-2 Ignite our App

--- App.js
+++ App.js
@@ -1,15 +1,6 @@
-/*
-    we'll create HTMl style nested things using react
-    <div id ="parent">
-        <div id ="child">
-            <h1>I am a h1 tag</h1>
-            <h2>I am a h2 tag</h2>
-        </div>
-    </div>
+import React from "react";
+import ReactDOM from "react-dom/client";
 
-
-
- */
 const parent=React.createElement("div",{id:"parent"},[
     React.createElement("div",{id:"child"},[
         React.createElement("h1",{},"I am a h1 tag"),
