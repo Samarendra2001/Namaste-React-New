650fe5bc6672cb62b3c8d126e8d7576fbc3d41f9
Samrendra Barick <baricksamarendra@gmail.com>
2023-11-24 15:33:27 +0530

    Chapter-3 -Laying the foundation

--- App.js
+++ App.js
@@ -1,19 +1,24 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
-const parent=React.createElement("div",{id:"parent"},[
-    React.createElement("div",{id:"child"},[
-        React.createElement("h1",{},"I am a h1 tag"),
-        React.createElement("h2",{},"I am a h2 tag"),
-    ])
-])
-console.log(parent);
-
-// this above code gives the exact output of Html code what is written in html above. If you want to write nested so write like this 
-const heading = React.createElement("h1",
-{id:"heading"},"Hello World From React");
+ const jsxheading = <h1 className = "heading">Namste React Using Jsx 🚀🚀</h1>//it is a react Element 
+/* const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(jsxheading); */
+// React functional Components
+//2 way of writting Functional Components both are Valid
+const num = 10000;
+const HeadingComponents =  () =>{
+    return <h1 className="heading">NamsteReact 2</h1>
+}
+const HeadingComponents2 = ()=>(
+    <>
+    {jsxheading}       {/* React element is used in Components by only curly */}
+    <h2>{num}</h2> {/* in that curly braces you can write any JavaScript Code and It will work */}
+    {HeadingComponents()}  {/* At the ene of the day it is all javascript so we call function like this also */}
+    <HeadingComponents></HeadingComponents> {/* these all three are same you can use it in whatever style */}
+    <HeadingComponents />              {/* Component Composition it is like use another component in a component */}
+    <h1 className="heading2">NamasteReact</h1>
+    </>
+)
 const root = ReactDOM.createRoot(document.getElementById("root"));
-//root.render(heading);
-root.render(parent);
-root.render(heading);
-
+root.render(<HeadingComponents2/>);
